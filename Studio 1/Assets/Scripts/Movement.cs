2592cc31178ede7250a44d99671194af8dd3b699
Kyle Fernandes <12rmaker@gmail.com>
2019-10-15 00:07:24 +0400

    Working on movement script, still bugs present to work out

---
+++ Studio 1/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 movement;
+    public int speed;
+    public float gravity = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxis("Horizontal") * speed;
+        float v = Input.GetAxis("Vertical") * speed;
+
+        movement = new Vector3(h, 0, v);
+        movement = Vector3.ClampMagnitude(movement, speed); //Limits speed
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement);
+    }
+}
