a3831b44d202ad0b09d1afb08f37fdc871a03bbc
lilrusty <r.dsouza@saedubai.com>
2019-10-15 12:55:36 +0400

    Basic Camera Movment(Temporary)

---
+++ Studio 1/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float rotSpeed;
+    float x;
+    float y;
+
+    public GameObject player;
+
+    void LateUpdate()
+    {
+        CamMove();
+    }
+
+    void CamMove()
+    {
+        x += Input.GetAxis("Mouse X") * rotSpeed;
+        y -= Input.GetAxis("Mouse Y") * rotSpeed;
+        y = Mathf.Clamp(y, -35, 80);
+
+        this.transform.LookAt(player.transform);
+
+        this.transform.rotation = Quaternion.Euler(y, x, 0);
+        player.transform.rotation = Quaternion.Euler(0, x, 0);
+    }
+}
