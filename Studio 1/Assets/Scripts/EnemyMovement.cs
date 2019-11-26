b07b34e5831cfad6ace2667f5beb2800ff64d03c
Christos257 <christos257@gmail.com>
2019-11-26 01:14:47 -0800

    Finished traps, added enemy movement and still working on wander script.

---
+++ Studio 1/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class EnemyMovement : Node
+{
+    public float speed = 60;
+    public Vector3 distance;
+    public Vector3 footstep;
+
+    public override void UpdateScreen(Manager man)
+    {
+        distance = man.patrol[man.index] - man.transform.position;
+        footstep = distance.normalized * speed * Time.deltaTime;
+        man.transform.position = man.transform.position + footstep;
+
+        if (distance.magnitude <= footstep.magnitude)
+        {
+            man.index++;
+            if (man.index == man.patrol.Length)
+            {
+                man.index = 0;
+            }
+        }
+    }
+}*/
---
+++ Studio 1/Assets/Scripts/Wander.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 circleCenter;
+    public Vector3 displacementForce;
+    public Vector3 wanderForce;
+    public float speed = 5f;
+    public float len;
+    public float circleDistance;
+    public float circleRadius;
+    public float wanderAngle;
+    public float angleChange;
+
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        displacementForce = new Vector3(0, 1, 0);
+        angleChange = 30;
+        CalcCircleCenter();
+        rb.velocity = rb.velocity * speed * Time.deltaTime;
+        transform.position = transform.position + rb.velocity;
+        
+    }
+    
+    void Update()
+    {
+        wanderAngle += (Random.Range(0f, 1f) * angleChange) - (angleChange * .5f);
+        setAngle(displacementForce, wanderAngle);
+        WanderForce();
+    }
+
+    public void CalcCircleCenter()
+    {
+        circleCenter = rb.velocity;
+        circleCenter = circleCenter.normalized * circleDistance;
+    }
+
+    public Vector3 WanderForce()
+    {
+        wanderForce = circleCenter + displacementForce;
+
+        return wanderForce;
+    }
+
+    public void setAngle(Vector3 displacement, float angle)
+    {
+        len = displacement.magnitude;
+        displacement.x = Mathf.Cos(angle) * len;
+        displacement.y = Mathf.Sin(angle) * len;
+    }
+}
