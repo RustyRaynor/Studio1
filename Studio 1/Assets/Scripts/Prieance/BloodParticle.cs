02cb16fb2109c1b52ff1eda72aebef693b6db9ba
Prieance <p.sagar@saedubai.com>
2019-12-15 14:43:44 +0400

    Added basic particle effect on knight collision, footsteps audio

---
+++ Studio 1/Assets/Scripts/Prieance/BloodParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodParticle : MonoBehaviour
+{
+    public Transform bleed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bleed.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Traps")
+        {
+            bleed.GetComponent<ParticleSystem>().enableEmission = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
